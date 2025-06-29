de93cbb4e03a9917c4c77830b397658c92d1618f
darpannemade <73686650+darpannemade@users.noreply.github.com>
2025-06-29 12:34:01 +0530

    Update HorizontalScroll.jsx

--- src/react_component/HorizontalScroll.jsx
+++ src/react_component/HorizontalScroll.jsx
@@ -30,7 +30,7 @@ const projects = [
     description: "Classifies tumors in brain using ensemble DL model of ResNet50+DenseNet201"
   },
   {
-    image: "/bg3jpg",
+    image: "/bg3.png",
     title: "Social Media Caption Generator",
     description: "CNN-LSTM Based social media post caption generator"
   },
